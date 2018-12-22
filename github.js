a153572c07b4c1e2593f7bd7f9bcda98a82856be
rekhansh99 <priyadarshirekhansh@gmail.com>
2018-12-22 20:21:14 +0530

    added to database

--- github.js
+++ github.js
@@ -1,8 +1,10 @@
 const octokit = require('@octokit/rest')();
-const fs = require('fs');
-const path = require('path');
+const mongo = require('mongodb').MongoClient;
 
-const users = [];
+const url = 'mongodb://localhost:27017/';     // Database URL
+const databaseName = 'DatabaseName';          // Enter database name
+const collectionName = 'CollectionName';      // Enter collection name
+
 const userNames = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'users.json')));
 const promises = [];
 
@@ -12,78 +14,31 @@ for (const year in userNames) {
       name: userName,
       year: year,
       repos: [],
-      totalCommits: 0,
-      weeklyCommits: []
+      weeklyCommits: new Array(52).fill(0)
     };
 
-    promises.push(octokit.repos.listForUser({
+    octokit.repos.listForUser({
       username: userName
     }).then(res => {
-      if (res.data)
-        res.data.forEach(repo => {
-          if (!repo.fork)
-            user.repos.push(repo.name);
-        });
+      res.data.forEach(repo => {
+        user.repos.push(repo.name);
+        promises.push(octokit.repos.getParticipationStats({
+          owner: user.name,
+          repo: repo.name
+        }).then(result => 
+          user.weeklyCommits.forEach((num, i) => user.weeklyCommits[i] += result.data.owner[i])));
+      });
 
-      users.push(user);
-    }));
+      Promise.all(promises).then(() => {
+        mongo.connect(url, (err, db) => {
+          if(err) throw err;
+          const dbo = db.db(databaseName);
+          dbo.collection(collectionName).insertOne(user, (err, res) => {
+            if(err) throw err;
+            db.close();
+          });
+        });
+      });
+    });
   });
 }
-
-Promise.all(promises).then(() => {
-  const p = [];
-  users.forEach(user => {
-    user.repos.forEach(repo => {
-      p.push(octokit.repos.getContributorsStats({
-        owner: user.name,
-        repo: repo
-      }).then(result => {
-        if (result.data)
-          result.data.forEach(data => {
-            u = users.find(user => user.name === data.author.login);
-            if (u) {
-              u.totalCommits += data.total;
-
-              for (let i = data.weeks.length - 1; i >= 0; i--) {
-                if (u.weeklyCommits.length > 0) {
-                  let inserted = false;
-
-                  for (let j = u.weeklyCommits.length - 1; j >= 0; j--) {
-                    if (data.weeks[i].w < u.weeklyCommits[j].w)
-                      continue;
-                    else if (data.weeks[i].w > u.weeklyCommits[j].w) {
-                      for (let k = u.weeklyCommits.length - 1; k > j; k--)
-                        u.weeklyCommits[k + 1] = u.weeklyCommits[k];
-                      u.weeklyCommits[j + 1] = data.weeks[i];
-                      inserted = true;
-                      break;
-                    } else {
-                      u.weeklyCommits[j].a += data.weeks[i].a;
-                      u.weeklyCommits[j].d += data.weeks[i].d;
-                      u.weeklyCommits[j].c += data.weeks[i].c;
-                      inserted = true;
-                      break;
-                    }
-                  }
-
-                  if (!inserted)
-                    u.weeklyCommits.unshift(data.weeks[i]);
-                } else
-                  u.weeklyCommits = data.weeks;
-              }
-            }
-          });
-      }));
-    });
-  });
-
-  Promise.all(p).then(() => {
-    users.forEach(user => {
-      console.log(user.name);
-      console.log(user.year);
-      console.log(user.repos);
-      console.log(user.totalCommits);
-      console.log(user.weeklyCommits);
-    });
-  });
-});
